fa71051e5a794ed29b9e3df0e2129f69d86d0adf
bruno <bruno.simon@getnet.com.br>
2020-01-28 14:26:55 -0300

    continuacao

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,11 +1,61 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/articles", (req, res) =>{
-    res.send("rota de artigos")
+router.get("/admin/articles", (req, res) =>{
+    Article.findAll(
+       { include:[{
+            model: Category,
+            required: true
+        }]}
+    ).then(articles =>{
+        
+        console.log(articles);
+        res.render("admin/articles/index",{articles: articles})
+    })
 })
 
 router.get("/admin/articles/new", (req, res) =>{
-    res.render("admin/articles/new");
+    Category.findAll().then(categories =>{
+        include:[{
+            model: Category
+        }]
+        res.render("admin/articles/new",{categories: categories});
+    })
 })
+router.post("/article/save", (req, res) =>{
+    var title = req.body.title;
+    var body = req.body.body;
+    console.log(body, 'body');
+    var category = req.body.category
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles");
+    });
+});
+router.post("/articles/delete", (req, res) =>{
+    const id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Article.destroy({
+                where:{
+                    id: id
+                }
+            }).then(() =>{
+                res.redirect("/admin/articles");
+            })
+        }else{ ///null
+            res.redirect("/admin/articles");
+        }
+    }else ///nul
+    {
+        res.redirect("/admin/articles");
+    }
+});
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
 const conection = new Sequelize('estudosNode', 'root', '',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = conection;
